8890f18b4af7d066408fefd26eea148ad6fecf74
Scott Newcomer <snewcomer24@gmail.com>
2025-02-02 06:07:57 -0600

    add all the things

---
+++ mix.exs
@@ -0,0 +1,48 @@
+defmodule NifExtractous.MixProject do
+  use Mix.Project
+
+  @version "0.0.1"
+  @source_url "https://github.com/snewcomer/nif-extractous"
+
+  def project do
+    [
+      app: :nif_extractous,
+      version: @version,
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      description: "A extractous rust precompiled",
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  # When publishing a library to with precompiled NIFs to Hex,
+  # is is mandatory to include a checksum file (along with other
+  # necessary files in the library).
+  #
+  defp package do
+    [
+      files: [
+        "lib",
+        "native",
+        "checksum-*.exs",
+        "mix.exs"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:rustler_precompiled, "~> 0.8"},
+      {:rustler, "~> 0.36.0", runtime: false}
+    ]
+  end
+end
---
+++ native/nifextractous/src/lib.rs
@@ -0,0 +1,14 @@
+use extractous::Extractor;
+use rustler::NifResult;
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn extract_document(path: String) -> NifResult<(String, String)> {
+    let extractor = Extractor::new();
+
+    match extractor.extract_file_to_string(&path) {
+        Ok((content, metadata)) => Ok((content, format!("{:?}", metadata))),
+        Err(e) => Err(rustler::Error::Term(Box::new(format!("Extraction failed: {}", e))))
+    }
+}
+
+rustler::init!("Elixir.NifExtractous");
