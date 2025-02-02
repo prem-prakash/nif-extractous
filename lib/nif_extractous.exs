b2474db9c9c777fc927f516354e5a6b35dcca5f9
Scott Newcomer <snewcomer24@gmail.com>
2025-02-02 11:12:38 -0600

    updates with right crate

--- lib/nif_extractous.exs
+++ lib/nif_extractous.exs
@@ -2,11 +2,11 @@ defmodule NifExtractous do
   version = Mix.Project.config()[:version]
 
   use RustlerPrecompiled,
-    otp_app: :radiant_intel,
-    crate: "doc_extractor",
+    otp_app: :nif_extractous,
+    crate: "nif_extractous",
     base_url:
-      "https://github.com/snewcomer/nif-extractous/releases/v#{version}",
-    force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
+      "https://github.com/snewcomer/nif-extractous/releases/download/v#{version}",
+    force_build: System.get_env("EXTRACTOUS_BUILD") in ["1", "true"],
     version: version
 
   def extract_document(_path), do: :erlang.nif_error(:nif_not_loaded)
