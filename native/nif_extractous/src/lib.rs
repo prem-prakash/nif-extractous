a3832db542b18a9858cceeb5c60cbd7d33629a5f
Scott Newcomer <snewcomer24@gmail.com>
2025-03-04 06:53:55 -0600

    add ocr method

--- native/nif_extractous/src/lib.rs
+++ native/nif_extractous/src/lib.rs
@@ -1,9 +1,19 @@
-use extractous::Extractor;
+use extractous::{Extractor, TesseractOcrConfig, PdfParserConfig, PdfOcrStrategy};
 use rustler::NifResult;
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn extract_document(path: String) -> NifResult<(String, String)> {
     let extractor = Extractor::new();
+
+    match extractor.extract_file_to_string(&path) {
+        Ok((content, metadata)) => Ok((content, format!("{:?}", metadata))),
+        Err(e) => Err(rustler::Error::Term(Box::new(format!("Extraction failed: {}", e))))
+    }
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn extract_document_ocr(path: String) -> NifResult<(String, String)> {
+    let extractor = Extractor::new().set_pdf_config(PdfParserConfig::new().set_ocr_strategy(PdfOcrStrategy::OCR_ONLY));
 
     match extractor.extract_file_to_string(&path) {
         Ok((content, metadata)) => Ok((content, format!("{:?}", metadata))),
