e53ea64527126d823778386083f140f0599bbf0b
Scott Newcomer <snewcomer24@gmail.com>
2025-03-04 10:00:35 -0600

    fix OCR strategy

--- native/nif_extractous/src/lib.rs
+++ native/nif_extractous/src/lib.rs
@@ -13,7 +13,11 @@ fn extract_document(path: String) -> NifResult<(String, String)> {
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn extract_document_ocr(path: String) -> NifResult<(String, String)> {
-    let extractor = Extractor::new().set_pdf_config(PdfParserConfig::new().set_ocr_strategy(PdfOcrStrategy::OCR_ONLY));
+    let extractor = Extractor::new()
+        .set_ocr_config(TesseractOcrConfig::new())
+        .set_pdf_config(
+            PdfParserConfig::new().set_ocr_strategy(PdfOcrStrategy::OCR_AND_TEXT_EXTRACTION).set_extract_inline_images(false).set_extract_unique_inline_images_only(false),
+        );
 
     match extractor.extract_file_to_string(&path) {
         Ok((content, metadata)) => Ok((content, format!("{:?}", metadata))),
